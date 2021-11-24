4fe81b57795922642e222294989fab9924fb20f6
Melvin <melvin.kucuk@fluxit.com.ar>
2021-11-24 15:12:24 -0300

    Initial commit

---
+++ app/src/main/java/com/alkemy/ongandroid/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.alkemy.ongandroid.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.alkemy.ongandroid.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
