5b4c81f32e0892b4d090981f73a406e5853d4f4c
Lucas Felici <lucas.a.felici@gmail.com>
2021-11-26 17:56:43 -0300

    Agregada la redireccion, directa por el momento

--- app/src/main/java/com/alkemy/ongandroid/view/LoginActivity.kt
+++ app/src/main/java/com/alkemy/ongandroid/view/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.alkemy.ongandroid.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.alkemy.ongandroid.R
 import com.alkemy.ongandroid.databinding.ActivityLoginBinding
@@ -21,6 +22,21 @@ class LoginActivity : AppCompatActivity() {
         binding.btnSignUp.setOnClickListener {
             navigateToSignUpScreen()
         }
+
+        binding.btnLogin.setOnClickListener{
+
+            val resp:Boolean = true
+
+            //funcion que le pega a la api
+            //resp[0].success -> True
+            //El else rompe porque si no es success no devuelve la misma data
+            if(resp){
+                navigateToMainScreen()
+            }else{
+                Toast.makeText(this,"Usuario o Contraseña incorrectos.",Toast.LENGTH_LONG).show()
+            }
+
+        }
     }
     private fun navigateToSignUpScreen() {
         val intent = Intent(this, SignUpActivity::class.java)
@@ -30,4 +46,10 @@ class LoginActivity : AppCompatActivity() {
         finishAffinity()
         finish()
     }
+
+    private fun navigateToMainScreen(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
 }
