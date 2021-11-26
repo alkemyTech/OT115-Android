cbfe206f5fa68487e33d609c2857683db6afd05f
GSaucedo <g.saucedo.kd@gmail.com>
2021-11-26 12:45:12 -0600

    Added coroutine to show message after 5 seconds

--- app/src/main/java/com/alkemy/ongandroid/view/SplashActivity.kt
+++ app/src/main/java/com/alkemy/ongandroid/view/SplashActivity.kt
@@ -1,11 +1,28 @@
 package com.alkemy.ongandroid.view
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.alkemy.ongandroid.R
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
+    private val DELAY_TIME = 5000L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startTimer()
+    }
+
+    private fun startTimer() = GlobalScope.launch {
+        delay(DELAY_TIME)
+        runOnUiThread {
+            Toast.makeText(
+                applicationContext,
+                getString(R.string.timer_message),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
