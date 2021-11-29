bd5589cb316e2e758ebbc1787fb6ecd37948d5d2
Mauricio Malsenido <mauriciomalsenido@gmail.com>
2021-11-29 15:25:24 -0300

    Se realizaron las correcciones solicitadas por el mentor en el primer comentario del pull request #9 "OT115-22 Validación de campos en la pantalla Sign Up"

--- app/src/main/java/com/alkemy/ongandroid/view/SignUpActivity.kt
+++ app/src/main/java/com/alkemy/ongandroid/view/SignUpActivity.kt
@@ -2,22 +2,26 @@ package com.alkemy.ongandroid.view
 
 import android.graphics.Color
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
 import android.util.Patterns
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.alkemy.ongandroid.R
-import com.alkemy.ongandroid.databinding.ActivityLoginBinding
-import com.alkemy.ongandroid.databinding.ActivityMainBinding
 import com.alkemy.ongandroid.databinding.ActivitySignUpBinding
 import java.util.regex.Pattern
 
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignUpBinding
+
+    companion object{
+        private const val SPECIAL_CHARACTERS_REGEX = "?=.*[\\u0020-\\u002F\\u003A-\\u0040\\u005B-\\u0060\\u007B-\\u007E]"
+        private const val PASSWORD_REGEX = "^" +
+                "(?=.*[0-9])" +                 //at least 1 digit
+                "(?=.*[a-zA-Z])" +              //any letter
+                "($SPECIAL_CHARACTERS_REGEX)" + //at least 1 special character
+                ".{4,}\$"                       //at least 4 characters
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,26 +33,10 @@ class SignUpActivity : AppCompatActivity() {
     private fun initializeComponents()
     {
         disableSaveButton()
-        binding.etUsername.setOnFocusChangeListener(object : View.OnFocusChangeListener {
-            override fun onFocusChange(v: View?, hasFocus: Boolean) {
-                validateFields()
-            }
-        })
-        binding.etEmail.setOnFocusChangeListener(object : View.OnFocusChangeListener {
-            override fun onFocusChange(v: View?, hasFocus: Boolean) {
-                validateFields()
-            }
-        })
-        binding.etPassword.setOnFocusChangeListener(object : View.OnFocusChangeListener {
-            override fun onFocusChange(v: View?, hasFocus: Boolean) {
-                validateFields()
-            }
-        })
-        binding.etConfirmPassword.setOnFocusChangeListener(object : View.OnFocusChangeListener {
-            override fun onFocusChange(v: View?, hasFocus: Boolean) {
-                validateFields()
-            }
-        })
+        binding.etUsername.onFocusChangeListener = View.OnFocusChangeListener { _, _ -> validateFields() }
+        binding.etEmail.onFocusChangeListener = View.OnFocusChangeListener { _, _ -> validateFields() }
+        binding.etPassword.onFocusChangeListener = View.OnFocusChangeListener { _, _ -> validateFields() }
+        binding.etConfirmPassword.onFocusChangeListener = View.OnFocusChangeListener { _, _ -> validateFields() }
     }
 
     private fun validateFields()
@@ -72,7 +60,7 @@ class SignUpActivity : AppCompatActivity() {
             disableSaveButton()
             if (!passwordsEquals)
             {
-                binding.tilConfirmPassword.error = "Passwords are not the same"
+                binding.tilConfirmPassword.error = getString(R.string.error_passwords_matches)
                 binding.tilConfirmPassword.isErrorEnabled = true
             }
         }
@@ -90,13 +78,7 @@ class SignUpActivity : AppCompatActivity() {
         binding.btnSaveUser.setBackgroundColor(ContextCompat.getColor(applicationContext,R.color.ong_color))
     }
 
-    fun isValidPasswordFormat(password: String): Boolean {
-        val SPECIAL_CHARACTERS_REGEX = "?=.*[\\u0020-\\u002F\\u003A-\\u0040\\u005B-\\u0060\\u007B-\\u007E]"
-        val PASSWORD_REGEX = "^" +
-                "(?=.*[0-9])" +                 //at least 1 digit
-                "(?=.*[a-zA-Z])" +              //any letter
-                "($SPECIAL_CHARACTERS_REGEX)" + //at least 1 special character
-                ".{4,}\$"                       //at least 4 characters
+    private fun isValidPasswordFormat(password: String): Boolean {
         return Pattern.matches(PASSWORD_REGEX, password)
     }
 }
