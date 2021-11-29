2a2c28f13aa1f1171b90f0aaf97675d714002d31
mardbel <martindelbel@gmail.com>
2021-11-29 10:42:26 -0300

    Api fixes

--- app/src/main/java/com/alkemy/ongandroid/businesslogic/api/OngApiService.kt
+++ app/src/main/java/com/alkemy/ongandroid/businesslogic/api/OngApiService.kt
@@ -2,7 +2,6 @@ package com.alkemy.ongandroid.businesslogic.api
 
 import com.alkemy.ongandroid.model.NewUserResponse
 import com.alkemy.ongandroid.model.User
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
