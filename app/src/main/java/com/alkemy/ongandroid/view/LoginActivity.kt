cb8f3d492bd6174399d7ebd99d7be6e55ec37268
Pablo Beltran <pablonqn55@gmail.com>
2021-11-29 23:54:26 -0300

    login layout refactor

--- app/src/main/java/com/alkemy/ongandroid/view/LoginActivity.kt
+++ app/src/main/java/com/alkemy/ongandroid/view/LoginActivity.kt
@@ -3,9 +3,7 @@ package com.alkemy.ongandroid.view
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.alkemy.ongandroid.R
 import com.alkemy.ongandroid.databinding.ActivityLoginBinding
-
 
 class LoginActivity : AppCompatActivity() {
 
--- app/src/main/java/com/alkemy/ongandroid/view/SplashActivity.kt
+++ app/src/main/java/com/alkemy/ongandroid/view/SplashActivity.kt
@@ -1,6 +1,5 @@
 package com.alkemy.ongandroid.view
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
