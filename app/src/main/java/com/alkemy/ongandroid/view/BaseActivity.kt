4e2957a86f894d0f9967a165ef4586ea4d01a81b
GSaucedo <g.saucedo.kd@gmail.com>
2021-12-01 15:19:02 -0600

    Added ProgressBar from xml layout and center it

--- app/src/main/java/com/alkemy/ongandroid/view/BaseActivity.kt
+++ app/src/main/java/com/alkemy/ongandroid/view/BaseActivity.kt
@@ -1,21 +1,34 @@
 package com.alkemy.ongandroid.view
 
 import android.view.View
-import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import com.alkemy.ongandroid.R
 
 open class BaseActivity : AppCompatActivity() {
     private lateinit var progressBar: ProgressBar
 
-    fun attachLoadingProgressBar(view: ViewGroup) {
-        progressBar = ProgressBar(applicationContext)
-        progressBar.layoutParams = LinearLayout.LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-        progressBar.visibility = View.GONE
+    fun attachLoadingProgressBar(view: ConstraintLayout) {
+        progressBar = layoutInflater.inflate(R.layout.custom_progress_bar, null) as ProgressBar
+
+        progressBar.apply {
+            val constraintLayoutParams: ConstraintLayout.LayoutParams =
+                ConstraintLayout.LayoutParams(
+                    ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                    ConstraintLayout.LayoutParams.WRAP_CONTENT
+                )
+            constraintLayoutParams.apply {
+                bottomToBottom = ConstraintSet.PARENT_ID
+                endToEnd = ConstraintSet.PARENT_ID
+                startToStart = ConstraintSet.PARENT_ID
+                topToTop = ConstraintSet.PARENT_ID
+            }
+
+            layoutParams = constraintLayoutParams
+        }
+
 
         view.addView(progressBar)
     }
