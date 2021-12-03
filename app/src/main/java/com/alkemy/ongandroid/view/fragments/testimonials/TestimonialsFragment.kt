488798af7a03bcffd9dcb16f85c5cef0fed56f81
GSaucedo <g.saucedo.kd@gmail.com>
2021-12-02 18:54:08 -0600

    Added onCreateView - Added onCreateView and fix liss parameters order

--- app/src/main/java/com/alkemy/ongandroid/view/fragments/testimonials/TestimonialsFragment.kt
+++ app/src/main/java/com/alkemy/ongandroid/view/fragments/testimonials/TestimonialsFragment.kt
@@ -1,9 +1,10 @@
 package com.alkemy.ongandroid.view.fragments.testimonials
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.alkemy.ongandroid.databinding.TestimonialsFragmentBinding
 import com.alkemy.ongandroid.model.Testimonial
 
@@ -12,10 +13,17 @@ class TestimonialsFragment : Fragment() {
     private lateinit var binding: TestimonialsFragmentBinding
     private lateinit var testimonialList: List<Testimonial>
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = TestimonialsFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = TestimonialsFragmentBinding.bind(view)
-
         loadOfflineData()
         binding.recyclerView.adapter = TestimonialsAdapter(testimonialList)
 
@@ -24,35 +32,35 @@ class TestimonialsFragment : Fragment() {
     private fun loadOfflineData() {
         testimonialList = listOf(
             Testimonial(
-                "Marita Gomez",
                 "http://ongapi.alkemy.org/storage/Mb7qJKMAWA.jpeg",
+                "Marita Gomez",
                 "Acompañamos el proceso de transformación de las comunidades promoviendo la participación activa de todos sus integrantes como sujetos."
             ),
             Testimonial(
-                "Josefa Prospero",
                 "http://ongapi.alkemy.org/storage/vpXetNTugz.jpeg",
+                "Josefa Prospero",
                 "Una mención especial por el excelente trabajo académico realizado por mi tutor, el Dr. Walter Castro Aponte, agradeciéndole igualmente a él."
             ),
             Testimonial(
-                "Marita Gomez",
                 "http://ongapi.alkemy.org/storage/Mb7qJKMAWA.jpeg",
+                "Marita Gomez",
                 "Acompañamos el proceso de transformación de las comunidades promoviendo la participación activa de todos sus integrantes como sujetos."
             ),
             Testimonial(
-                "Josefa Prospero",
-                "http://ongapi.alkemy.org/storage/vpXetNTugz.jpeg",
-                "Una mención especial por el excelente trabajo académico realizado por mi tutor, el Dr. Walter Castro Aponte, agradeciéndole igualmente a él."
-            ),
-            Testimonial(
-                "Marita Gomez",
                 "http://ongapi.alkemy.org/storage/Mb7qJKMAWA.jpeg",
+                "Marita Gomez",
                 "Acompañamos el proceso de transformación de las comunidades promoviendo la participación activa de todos sus integrantes como sujetos."
             ),
             Testimonial(
-                "Josefa Prospero",
                 "http://ongapi.alkemy.org/storage/vpXetNTugz.jpeg",
+                "Josefa Prospero",
                 "Una mención especial por el excelente trabajo académico realizado por mi tutor, el Dr. Walter Castro Aponte, agradeciéndole igualmente a él."
-            )
+            ),
+            Testimonial(
+                "http://ongapi.alkemy.org/storage/Mb7qJKMAWA.jpeg",
+                "Marita Gomez",
+                "Acompañamos el proceso de transformación de las comunidades promoviendo la participación activa de todos sus integrantes como sujetos."
+            ),
         )
     }
 }
