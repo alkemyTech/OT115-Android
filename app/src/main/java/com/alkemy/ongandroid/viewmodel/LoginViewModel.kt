0de233c38d9103f5924b4ea11ad30b2a04012627
lucas felici <lugas_felici@gmail.com>
2021-12-03 17:06:55 -0300

    Api Login Refactor complete

--- app/src/main/java/com/alkemy/ongandroid/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/alkemy/ongandroid/viewmodel/LoginViewModel.kt
@@ -1,24 +1,19 @@
 package com.alkemy.ongandroid.viewmodel
 
-import android.util.Log
 import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.alkemy.ongandroid.api.ApiONGImp
 import com.alkemy.ongandroid.businesslogic.PASSWORD_REGEX_WO_EC
 import com.alkemy.ongandroid.businesslogic.managers.LocalDataManager
 import com.alkemy.ongandroid.model.LoginData
 import com.alkemy.ongandroid.model.NewUserResponse
-import com.alkemy.ongandroid.model.ResponseLogin
 import com.alkemy.ongandroid.model.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import retrofit2.Call
-import retrofit2.awaitResponse
 import java.util.regex.Pattern
 import javax.inject.Inject
 
@@ -38,9 +33,8 @@ class LoginViewModel @Inject constructor(
         get() = _state
 
     private val _viewState = MutableLiveData(false)
-    val viewState : LiveData<Boolean>
+    val viewState: LiveData<Boolean>
         get() = _viewState
-
 
     private val _loginfo = MutableLiveData<MutableList<NewUserResponse>>()
     val loginfo: LiveData<MutableList<NewUserResponse>>
@@ -50,29 +44,13 @@ class LoginViewModel @Inject constructor(
     val progressBarStatus
         get() = _progressBarStatus
 
-
-//    private fun getLogin(email: String, pass: String): Call<ResponseLogin> {
-//        return ApiONGImp().login(email, pass)
-//    }
-
     fun login(email: String, pass: String) {
         _progressBarStatus.value = true
         viewModelScope.launch(Dispatchers.IO) {
-/*            val resp = getLogin(email, pass).awaitResponse()
-            if (resp.isSuccessful) {
-                val info = resp.body()
-                if (info?.data != null) {
-                    localDataManager.saveToken(info.data.token)
-                    withContext(Dispatchers.Main) {
-                        _state.value = State.Success
-                    }
-                }
-            }*/
-            val resp = repository.logUser(LoginData(email,pass))
-            if(resp.success){
-                localDataManager.saveToken(resp.data.token)
-                Log.e("token: ", resp.data.token)
 
+            val resp = repository.logUser(LoginData(email, pass))
+            if (resp.success) {
+                localDataManager.saveToken(resp.data.token)
             }
             withContext(Dispatchers.Main) {
                 _state.value = State.Success
@@ -89,8 +67,6 @@ class LoginViewModel @Inject constructor(
         val passwordsFormat: Boolean = Pattern.matches(PASSWORD_REGEX_WO_EC, password)
 
         _viewState.value = !fieldsEmpty && emailFormat && passwordsFormat
-        //_viewState.value = true
-
     }
 
 }
