67fa8bb8c9e4f834cb53d043055236f127d740c8
GSaucedo <g.saucedo.kd@gmail.com>
2021-12-03 13:42:58 -0600

    Created view model with harcoded data

---
+++ app/src/main/java/com/alkemy/ongandroid/viewmodel/TestimonialsFragmentViewModel.kt
@@ -0,0 +1,50 @@
+package com.alkemy.ongandroid.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.alkemy.ongandroid.model.Testimonial
+
+class TestimonialsFragmentViewModel : ViewModel() {
+    private val _testimonialList = MutableLiveData<List<Testimonial>>(arrayListOf())
+    val testimonialList: LiveData<List<Testimonial>> get() = _testimonialList
+
+    init {
+        fetchData()
+    }
+
+    private fun fetchData() {
+        _testimonialList.value = listOf(
+            Testimonial(
+                "http://ongapi.alkemy.org/storage/Mb7qJKMAWA.jpeg",
+                "Marita Gomez",
+                "Acompañamos el proceso de transformación de las comunidades promoviendo la participación activa de todos sus integrantes como sujetos."
+            ),
+            Testimonial(
+                "http://ongapi.alkemy.org/storage/vpXetNTugz.jpeg",
+                "Josefa Prospero",
+                "Una mención especial por el excelente trabajo académico realizado por mi tutor, el Dr. Walter Castro Aponte, agradeciéndole igualmente a él."
+            ),
+            Testimonial(
+                "http://ongapi.alkemy.org/storage/Mb7qJKMAWA.jpeg",
+                "Marita Gomez",
+                "Acompañamos el proceso de transformación de las comunidades promoviendo la participación activa de todos sus integrantes como sujetos."
+            ),
+            Testimonial(
+                "http://ongapi.alkemy.org/storage/Mb7qJKMAWA.jpeg",
+                "Marita Gomez",
+                "Acompañamos el proceso de transformación de las comunidades promoviendo la participación activa de todos sus integrantes como sujetos."
+            ),
+            Testimonial(
+                "http://ongapi.alkemy.org/storage/vpXetNTugz.jpeg",
+                "Josefa Prospero",
+                "Una mención especial por el excelente trabajo académico realizado por mi tutor, el Dr. Walter Castro Aponte, agradeciéndole igualmente a él."
+            ),
+            Testimonial(
+                "http://ongapi.alkemy.org/storage/Mb7qJKMAWA.jpeg",
+                "Marita Gomez",
+                "Acompañamos el proceso de transformación de las comunidades promoviendo la participación activa de todos sus integrantes como sujetos."
+            ),
+        )
+    }
+}
