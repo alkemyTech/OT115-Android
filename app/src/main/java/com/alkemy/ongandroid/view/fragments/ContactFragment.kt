2fb679bee107d52ee3330869e5eb32623471b016
GSaucedo <g.saucedo.kd@gmail.com>
2021-12-14 04:56:22 -0600

    Implemented binding

--- app/src/main/java/com/alkemy/ongandroid/view/fragments/ContactFragment.kt
+++ app/src/main/java/com/alkemy/ongandroid/view/fragments/ContactFragment.kt
@@ -6,28 +6,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.alkemy.ongandroid.viewmodel.ContactViewModel
 import com.alkemy.ongandroid.R
+import com.alkemy.ongandroid.databinding.ContactFragmentBinding
 
 class ContactFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ContactFragment()
-    }
-
-    private lateinit var viewModel: ContactViewModel
+    private val viewModel by viewModels<ContactViewModel>()
+    private lateinit var binding: ContactFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.contact_fragment, container, false)
+    ): View {
+        binding = ContactFragmentBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ContactViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
 }
