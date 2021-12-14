8f4b03757bec1b7b634144deef4dd1009a074588
GSaucedo <g.saucedo.kd@gmail.com>
2021-12-14 04:38:16 -0600

    Created contact fragment

---
+++ app/src/main/java/com/alkemy/ongandroid/view/fragments/ContactFragment.kt
@@ -0,0 +1,33 @@
+package com.alkemy.ongandroid.view.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.alkemy.ongandroid.viewmodel.ContactViewModel
+import com.alkemy.ongandroid.R
+
+class ContactFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ContactFragment()
+    }
+
+    private lateinit var viewModel: ContactViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.contact_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ContactViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/alkemy/ongandroid/viewmodel/ContactViewModel.kt
@@ -0,0 +1,7 @@
+package com.alkemy.ongandroid.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class ContactViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
