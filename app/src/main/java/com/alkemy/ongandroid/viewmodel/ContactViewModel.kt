36d18703dea49dc5eb0bec2bbbd2dffb377c5daa
Gerardo Saucedo Arevalo <77474094+GSaucedoA@users.noreply.github.com>
2021-12-15 16:21:44 -0600

    Feature/ot115 80 validate contact form (#33)
    * Added contact option to menu
    
    * Created contact fragment
    
    * Added ContactFragment to itemListener
    
    * Implemented binding
    
    * Cretaed contact design
    
    * Added string for contact screen
    
    * Replaced text view to show app logo
    
    * Added messages for invalid inputs
    
    * Validated all fields
    
    * - Added observers
    - Created custom text watcher
    - Disabled button when there is no valid data
    
    * - Added scroll view
    - Added id in each field to access to input text
    
    * - Added light version for ong_color
    
    * - Changed background colot to use light version of ong_color

--- app/src/main/java/com/alkemy/ongandroid/viewmodel/ContactViewModel.kt
+++ app/src/main/java/com/alkemy/ongandroid/viewmodel/ContactViewModel.kt
@@ -1,7 +1,31 @@
 package com.alkemy.ongandroid.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.alkemy.ongandroid.businesslogic.managers.Validator
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+@HiltViewModel
+class ContactViewModel @Inject constructor(private val validator: Validator) : ViewModel() {
+    private val _canSubmit = MutableLiveData(false)
+    val canSubmit: LiveData<Boolean> get() = _canSubmit
 
-class ContactViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _isFullNameValid = MutableLiveData<Boolean>()
+    val isFullNameValid: LiveData<Boolean> get() = _isFullNameValid
+
+    private val _isValidEmail = MutableLiveData<Boolean>()
+    val isValidEmail: LiveData<Boolean> get() = _isValidEmail
+
+    private val _isMessageValid = MutableLiveData<Boolean>()
+    val isMessageValid: LiveData<Boolean> get() = _isMessageValid
+
+
+    fun validateFields(fullName: String, email: String, message: String) {
+        _isFullNameValid.value = fullName.isNotEmpty() && fullName.isNotBlank()
+        _isMessageValid.value = message.isNotEmpty() && message.isNotBlank()
+        _isValidEmail.value = validator.validateEmail(email)
+        _canSubmit.value =
+            isFullNameValid.value == true && isValidEmail.value == true && isMessageValid.value == true
+    }
 }
