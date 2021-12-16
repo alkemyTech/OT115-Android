c93f0d1d8133453bcad4e21565ac50457caf0b7b
“Melvin” <melvinalek@gmail.com>
2021-12-16 19:02:56 -0300

    refactor

--- app/src/main/java/com/alkemy/ongandroid/viewmodel/ApiDataViewModel.kt
+++ app/src/main/java/com/alkemy/ongandroid/viewmodel/ApiDataViewModel.kt
@@ -1,14 +1,16 @@
 package com.alkemy.ongandroid.viewmodel
 
-import androidx.lifecycle.*
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import com.alkemy.ongandroid.businesslogic.repositories.ApiRepo
 import com.alkemy.ongandroid.core.Response
-import com.alkemy.ongandroid.businesslogic.repositories.ApiRepoImpl
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
 @HiltViewModel
-class ApiDataViewModel @Inject constructor (private val repo: ApiRepoImpl): ViewModel() {
+class ApiDataViewModel @Inject constructor(private val repo: ApiRepo) : ViewModel() {
 
 
     fun getNews() = liveData(viewModelScope.coroutineContext + Dispatchers.IO) {
@@ -16,7 +18,7 @@ class ApiDataViewModel @Inject constructor (private val repo: ApiRepoImpl): View
         emit(Response.Loading())
         try {
             emit(Response.Success(repo.getNews()))
-        }catch (e:Throwable){
+        } catch (e: Throwable) {
             emit(Response.Failure(e))
         }
     }
