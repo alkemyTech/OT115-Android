9b89f3076600beff93a68d444e5e71e9be03976d
GSaucedo <g.saucedo.kd@gmail.com>
2021-12-22 10:09:42 -0600

    Tested all posible cases

--- app/src/test/java/com/alkemy/ongandroid/viewmodel/TestimonialsFragmentViewModelTest.kt
+++ app/src/test/java/com/alkemy/ongandroid/viewmodel/TestimonialsFragmentViewModelTest.kt
@@ -1,15 +1,20 @@
 package com.alkemy.ongandroid.viewmodel
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.alkemy.ongandroid.MainCoroutineRule
 import com.alkemy.ongandroid.businesslogic.repositories.ApiRepo
-import com.alkemy.ongandroid.businesslogic.repositories.UserRepository
+import com.alkemy.ongandroid.model.ApiTestimonialsResponse
 import com.alkemy.ongandroid.util.getOrAwaitValue
 import junit.framework.TestCase
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestRule
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 
@@ -17,17 +22,60 @@ import org.mockito.kotlin.whenever
 @RunWith(JUnit4::class)
 class TestimonialsFragmentViewModelTest : TestCase() {
 
-    private val apiRepo: ApiRepo = mock()
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val coroutineRule = MainCoroutineRule()
+
+    private val apiRepoMock: ApiRepo = mock()
     private lateinit var viewModel: TestimonialsFragmentViewModel
+    private val successResponse = ApiTestimonialsResponse(true, emptyList())
+
+    private val failureResponse = mock<ApiTestimonialsResponse> {
+        on { success } doReturn false
+        on { data } doReturn emptyList()
+    }
 
     @Before
     public override fun setUp() {
-        viewModel = TestimonialsFragmentViewModel(apiRepo)
+        viewModel = TestimonialsFragmentViewModel(apiRepoMock)
     }
 
     @Test
-    fun `Should set loading state to false when finished` () {
+    fun `Should set loading state to false when api call finished`() = runBlocking {
+        whenever(apiRepoMock.getTestimonials()).thenReturn(successResponse)
         viewModel.getTestimonials()
-        assertEquals(false, viewModel.loadingState.getOrAwaitValue())
+        assertFalse(viewModel.loadingState.getOrAwaitValue())
     }
+
+    @Test
+    fun `Should set loadingState to true at least once`() = runBlocking {
+        whenever(apiRepoMock.getTestimonials()).thenReturn(successResponse)
+        viewModel.loadingState.observeForever {
+            assertTrue(it)
+        }
+        viewModel.getTestimonials()
+    }
+
+    @Test
+    fun `Should set state to Success when api call finished`() = runBlocking {
+        whenever(apiRepoMock.getTestimonials()).thenReturn(successResponse)
+        viewModel.getTestimonials()
+        assertEquals(
+            TestimonialsFragmentViewModel.State.Success(successResponse).javaClass,
+            viewModel.state.getOrAwaitValue().javaClass
+        )
+    }
+
+    @Test
+    fun `Should set state to Failure when api call finished`() = runBlocking {
+        whenever(apiRepoMock.getTestimonials()).thenReturn(failureResponse)
+        viewModel.getTestimonials()
+        assertEquals(
+            TestimonialsFragmentViewModel.State.Failure(Throwable()).javaClass,
+            viewModel.state.getOrAwaitValue().javaClass
+        )
+    }
+
 }
