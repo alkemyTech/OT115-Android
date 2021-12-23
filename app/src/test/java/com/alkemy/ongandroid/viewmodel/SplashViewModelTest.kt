2cbf08a5240b1e761893706f36b9f225f3923b5e
Mauricio Malsenido <mauriciomalsenido@gmail.com>
2021-12-23 01:37:18 -0300

    Realización de las Unit Tests correspondientes a SplashViewModel.kt.

---
+++ app/src/test/java/com/alkemy/ongandroid/viewmodel/SplashViewModelTest.kt
@@ -0,0 +1,46 @@
+package com.alkemy.ongandroid.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.alkemy.ongandroid.businesslogic.managers.LocalDataManager
+import com.alkemy.ongandroid.util.getOrAwaitValue
+import junit.framework.Assert.assertFalse
+import junit.framework.Assert.assertTrue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+@ExperimentalCoroutinesApi
+@RunWith(JUnit4::class)
+class SplashViewModelTest {
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    private val localDataManager: LocalDataManager = mock()
+    private lateinit var viewModel: SplashViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = SplashViewModel(localDataManager)
+    }
+
+    @Test
+    fun `get token with an non-empty string returned`(){
+        whenever(localDataManager.getToken()).thenReturn("UserToken")
+        viewModel.getToken()
+        assertTrue(viewModel.existToken.getOrAwaitValue())
+    }
+
+    @Test
+    fun `get token with an empty string returned`(){
+        whenever(localDataManager.getToken()).thenReturn("")
+        viewModel.getToken()
+        assertFalse(viewModel.existToken.getOrAwaitValue())
+    }
+}
